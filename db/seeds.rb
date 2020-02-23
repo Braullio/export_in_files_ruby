2469da9050dbf9933e627a4ccf893cbfba550270
Braullio Vinnicius <braulliovg@gmail.com>
2020-02-23 00:06:01 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+  name = Faker::Coffee.blend_name
+  quantity = rand(0..100)
+  Item.create(name: name, quantity: quantity)
+end
