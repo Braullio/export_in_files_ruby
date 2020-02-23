6435b4694219a01a12e15c39deab7f4231a76957
Braullio Vinnicius <braulliovg@gmail.com>
2020-02-23 00:22:23 -0300

    update filename xlsx

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ def index
     format.xlsx {
       response.headers[
         'Content-Disposition'
-      ] = "attachment; filename='items.xlsx'"
+      ] = "attachment; filename=items.xlsx"
     }
     format.html { render :index }
   end
